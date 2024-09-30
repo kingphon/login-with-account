5e44ae327ebbe17bfa9bad008cfe8a90d33e7e1b
kingphon <nguyenminhp2000vn@gmail.com>
2024-09-30 16:27:13 +0700

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,37 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import dts from "rollup-plugin-dts";
+import packageJson from "./package.json" assert { type: "json" };
+// const packageJson = require("./package.json");
+
+export default defineConfig([
+  // Main configuration for bundling your code
+  {
+    input: "src/index.ts",
+    // external: ["react-dom"],
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+]);
---
+++ src/components/LoginWithAccount/index.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+type Props = {
+  label?: string;
+  onLoginSuccess?: (code: string) => void;
+  onLoginError?: (message: string) => void;
+};
+
+const LoginWithAccount = (props: Props) => {
+  const { label = "Login with Account", onLoginSuccess, onLoginError } = props;
+  const onSubmit = () => {
+    const popupWidth = 500;
+    const popupHeight = 600;
+    const left = (window.innerWidth - popupWidth) / 2;
+    const top = (window.innerHeight - popupHeight) / 2;
+
+    const loginPopup = window.open(
+      `http://localhost:3001/?origin=${window.location.origin}`,
+      "Login",
+      `width=${popupWidth},height=${popupHeight},top=${top},left=${left}`,
+    );
+
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (event.data.type === "OAUTH_SUCCESS") {
+          onLoginSuccess?.(event.data.token);
+        }
+        if (event.data.type === "OAUTH_FAILED") {
+          onLoginError?.(event.data.message);
+        }
+      },
+      false,
+    );
+  };
+
+  return (
+    <button
+      onClick={onSubmit}
+      style={{
+        width: "100%",
+        background: "white",
+        color: "black",
+        padding: "10px 20px",
+        borderRadius: 10,
+      }}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default LoginWithAccount;
